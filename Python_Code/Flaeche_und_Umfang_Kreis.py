fd4f999ca826e30ae3fc714db76d422932bfa89f
TheGhodra <den.schnepf.23@lehre.mosbach.dhbw.de>
2023-12-13 19:00:36 +0100

    Day_8

---
+++ Python_Code/Flaeche_und_Umfang_Kreis.py
@@ -0,0 +1,8 @@
+print("Berechnungne am Kreis:")
+
+r = float(input("Gib den Radius ein: "))
+
+umfang = round(2 * 3.14 * r, 2)
+flaeche = round(3.14 * r**2, 2)
+
+print("Der Kreis hat einen Umfang von ", umfang, " und einen Flächeninhalt von ", flaeche)
