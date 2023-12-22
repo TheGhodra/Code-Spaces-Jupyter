54aefaf7a6ef210fddcf42bed5ec64906787db6a
TheGhodra <den.schnepf.23@lehre.mosbach.dhbw.de>
2023-12-22 11:56:18 +0100

    Day_12

--- Python_Code/Grafische_Oberflaeche.py
+++ Python_Code/Grafische_Oberflaeche.py
@@ -9,16 +9,16 @@ def button_action():
 fenster = Tk()
 
 # Den Fenstertitle erstellen
-fenster.title("Ich mache nun was")
+fenster.title("Grafische Oberfläche")
 
 #Label und Buttons ertsellen
 change_button = Button(fenster, text = "Ändern", command = button_action)
 exit_button = Button(fenster, text = "Beenden", command = fenster.quit)
 
-anweisungs_label = Label(fenster, text = "Ich bin eine Anweisung: \n\ Klicke auf 'Ändern'")
+anweisungs_label = Label(fenster, text = "Ich bin eine Anweisung: \n Klicke auf 'Ändern'")
 
 
-info_label = Label(fenster, text = "Ich bin eine Info: \n\ Der Beenden Button schließt das Programm")
+info_label = Label(fenster, text = "Ich bin eine Info: \n Der Beenden Button schließt das Programm")
 
 # Nun fügen wir die Komponenten unserem Fenster
 # in der gewünschten Reihenfolge zu
