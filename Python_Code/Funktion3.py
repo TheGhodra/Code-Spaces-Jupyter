72358c9cd09bf5d400c4d911a625f86c239c27ac
TheGhodra <den.schnepf.23@lehre.mosbach.dhbw.de>
2024-01-10 17:09:12 +0100

    Day_24

---
+++ Python_Code/Funktion3.py
@@ -0,0 +1,8 @@
+def ausgabe(anfangswert, endwert, schrittweite):
+    for x in range(anfangswert, endwert, schrittweite):
+        print(x)
+    print("Funktion ausgabe durchlaufen")
+
+ausgabe(4, 9, 2)
+ausgabe(-5, -8, -1)
+print("fertig")
