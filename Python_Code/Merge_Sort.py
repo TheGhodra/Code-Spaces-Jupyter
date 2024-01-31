c6dc0c2c330d3ae5ef71b63c1d41d05ef4ea251c
TheGhodra <den.schnepf.23@lehre.mosbach.dhbw.de>
2024-01-31 14:59:32 +0100

    Day_39

---
+++ Python_Code/Merge_Sort.py
@@ -0,0 +1,40 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        # Find the middle of the array
+        mid = len(arr) // 2
+        
+        # Divide the array into two halves
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        # Recursively sort the two halves
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        # Merge the sorted halves
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Check for any remaining elements
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Beispiel
+my_array = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(my_array)
+print("Sorted Array:", my_array)
